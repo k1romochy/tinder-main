96fb83aa6abfa3a7a5ee1c81538a96e1f9a71f4e
k1romochy@mail.ru@ <k1romochy@mail.ru>
2025-04-13 13:48:43 +0300

    create likes_service

--- src/main/java/com/example/demo/preferences/Repository/PreferencesRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.demo.preferences.Repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface PreferencesRepository extends JpaRepository<Preferences, Long> {
-}
--- src/main/java/com/example/demo/stack/Repositrory/StackRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.demo.stack.Repositrory;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface StackRepository extends JpaRepository<Stack, Long> {
-}
